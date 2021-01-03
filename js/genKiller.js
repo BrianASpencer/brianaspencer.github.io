561bf1a5300942e41cf08bc1723fd669a4733625
Brian Spencer <46761632+BrianASpencer@users.noreply.github.com>
2021-01-02 23:28:48 -0500

    Update genKiller.js

--- js/genKiller.js
+++ js/genKiller.js
@@ -11,7 +11,6 @@ const fetchKiller = (bound) => {
         const url = 'https://dbd-stats.info/api/characters';
         promises.push(fetch(url).then((res) => res.json()));
     }
-    console.log('lag', promises);
     Promise.all(promises).then((results) => {
         buildKiller(results);
     });
@@ -21,24 +20,22 @@ const fetchKiller = (bound) => {
 const buildKiller = (arr) => {
     arr.forEach(myFunction);
     function myFunction(item) {
-        var sPerks = [];
+        var killers = [];
         for (const element in item) {
-            var sIndex = 0;
             var url = 'https://dbd-stats.info/data/Public/';
             if (item[element].role === "EPlayerRole::VE_Slasher") {
-                sPerks.push([item[element].displayName, item[element].biography, url + item[element].iconPath]);
+                killers.push([item[element].displayName, item[element].biography, url + item[element].iconPath]);
             }
         }
-        console.log('lagging', sPerks);
         
-        return displayKiller(sPerks[getRandomInt(sPerks.length)]);
+        return displayKiller(killers[getRandomInt(killers.length)]);
     }
 };
 
-const displayKiller = (disPerks) => {
+const displayKiller = (killer) => {
     killerSlot.innerHTML = '';
     var lag = '<li class="list-group-item list-group-item-danger"><h2 class="text-center">' +
-    disPerks[0] + '</h2>' + '<div class="text-center"> <img style="width: 20%; height: 20%;" src="' + disPerks[2] +'"></div>' + '<div>' + disPerks[1].replace(/<(.|\n)*?>/g, '') + '</div><br>' + '</li>';
+    killer[0] + '</h2>' + '<div class="text-center"> <img style="width: 20%; height: 20%;" src="' + killer[2] +'"></div>' + '<div>' + killer[1].replace(/<(.|\n)*?>/g, '') + '</div><br>' + '</li>';
     killerSlot.innerHTML += lag;
 };
 
