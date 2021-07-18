65f1c1bd803289534dd1235746d1938737fb970b
Brian Spencer <46761632+BrianASpencer@users.noreply.github.com>
2021-07-18 10:01:24 -0400

    Add files via upload

---
+++ js/Data/killerTier.js
@@ -0,0 +1,164 @@
+const tierList = [{
+
+name: 'The Trapper',
+
+ rating:6.87
+
+},
+
+{
+
+name: 'The Hillbilly',
+
+ rating:8.27
+
+},
+
+{
+
+name: 'The Wraith',
+
+ rating:6.55
+
+},
+
+{
+
+name: 'The Nurse',
+
+ rating:9.6
+
+},
+
+{
+
+name: 'The Shape',
+
+ rating:6.52
+
+},
+
+{
+
+name: 'The Hag',
+
+ rating:7.77
+
+},
+
+{
+
+name: 'The Doctor',
+
+ rating:7.2
+
+},
+
+{
+
+name: 'The Huntress',
+
+ rating:7.83
+
+},
+
+{
+
+name: 'The Cannibal',
+
+ rating:6.38
+
+},
+
+{
+
+name: 'The Nightmare',
+
+ rating:8.55
+
+},
+
+{
+
+name: 'The Pig',
+
+ rating:6.37
+
+},
+
+{
+
+name: 'The Clown',
+
+ rating:5.22
+
+},
+
+{
+
+name: 'The Spirit',
+
+ rating:9.37
+
+},
+
+{
+
+name: 'The Legion',
+
+ rating:6.22
+
+},
+
+{
+
+name: 'The Plague',
+
+ rating:7.22
+
+},
+
+{
+
+name: 'The Ghost Face',
+
+ rating:7.03
+
+},
+
+{
+
+name: 'The Demogorgon',
+
+ rating:6.92
+
+},
+
+{
+
+name: 'The Oni',
+
+ rating:8.53
+
+},
+
+{
+
+name: 'The Deathslinger',
+
+ rating:7.38
+
+},
+
+{
+
+name: 'The Executioner',
+
+ rating:7.23
+
+}
+
+];
+
+
+
