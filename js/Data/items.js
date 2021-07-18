a375218acaaa583aed9848086ec9f8785f3f6def
Brian Spencer <46761632+BrianASpencer@users.noreply.github.com>
2021-07-18 12:00:53 -0400

    Update items.js

--- js/Data/items.js
+++ js/Data/items.js
@@ -1,30 +1,64 @@
 //Dir | Rename-Item –NewName { $_.name –replace “ “,”_” }
 const survivorItems = [
-	{
-		name: "Emergency Med-Kit",
-		image: "../../images/DbD/items/medkit.png",
-		id: "Item_Camper_MedKit03"
-	},
+	//KEYS
 	{
 		name: "Broken Key",
 		image: "../../images/DbD/items/brokenKey.png",
 		id: "Item_Camper_BrokenKey"
 	},
-	{
-		name: "Utility Flashlight",
-		image: "../../images/DbD/items/flashlightUtility.png",
-		id: "Item_Camper_Flashlight03"
+    {
+		name: "Broken Key",
+		image: "../../images/DbD/items/brokenKey.png",
+		id: "Item_Camper_BrokenKey"
 	},
-	{
-		name: "First Aid Kit",
-		image: "../../images/DbD/items/firstAidKit.png",
-		id: "Item_Camper_MedKit02"
+    {
+		name: "Broken Key",
+		image: "../../images/DbD/items/brokenKey.png",
+		id: "Item_Camper_BrokenKey"
 	},
+    {
+		name: "Broken Key",
+		image: "../../images/DbD/items/brokenKey.png",
+		id: "Item_Camper_BrokenKey"
+	},
+    
+    
+	
+	//MEDKITS
 	{
 		name: "Ranger Med-Kit",
 		image: "../../images/DbD/items/rangersAidKit.png",
 		id: "Item_Camper_MedKit04"
 	},
+    {
+		name: "Emergency Med-Kit",
+		image: "../../images/DbD/items/medkit.png",
+		id: "Item_Camper_MedKit03"
+	},
+    {
+		name: "First Aid Kit",
+		image: "../../images/DbD/items/firstAidKit.png",
+		id: "Item_Camper_MedKit02"
+	},
+    {
+		name: "Camping Aid Kit",
+		image: "../../images/DbD/items/rundownAidKit.png",
+		id: "Item_Camper_MedKit"
+	},
+    {
+		name: "Camping Aid Kit",
+		image: "../../images/DbD/items/rundownAidKit.png",
+		id: "Item_Camper_MedKit"
+	},
+    {
+		name: "Camping Aid Kit",
+		image: "../../images/DbD/items/rundownAidKit.png",
+		id: "Item_Camper_MedKit"
+	},
+    
+    
+    
+    //MAPS
 	{
 		name: "Rainbow Map",
 		image: "../../images/DbD/items/rainbowMap.png",
@@ -35,26 +69,55 @@ const survivorItems = [
 		image: "../../images/DbD/items/map.png",
 		id: "Item_Camper_BeigeMap"
 	},
-	{
-		name: "Camping Aid Kit",
-		image: "../../images/DbD/items/rundownAidKit.png",
-		id: "Item_Camper_MedKit"
+    {
+		name: "Map",
+		image: "../../images/DbD/items/map.png",
+		id: "Item_Camper_BeigeMap"
 	},
+    {
+		name: "Map",
+		image: "../../images/DbD/items/map.png",
+		id: "Item_Camper_BeigeMap"
+	},
+    
+    
+    
 	{
 		name: "Toolbox",
 		image: "../../images/DbD/items/toolbox.png",
 		id: "Item_Camper_Toolbox"
 	},
 	{
-		name: "Sport Flashlight",
-		image: "../../images/DbD/items/flashlightSport.png",
-		id: "Item_Camper_Flashlight02"
-	},
-	{
 		name: "Worn-out Tools",
 		image: "../../images/DbD/items/toolboxWornOut.png",
 		id: "Item_Camper_WornoutToolbox"
 	},
+    {
+		name: "Worn-out Tools",
+		image: "../../images/DbD/items/toolboxWornOut.png",
+		id: "Item_Camper_WornoutToolbox"
+	},
+    {
+		name: "Worn-out Tools",
+		image: "../../images/DbD/items/toolboxWornOut.png",
+		id: "Item_Camper_WornoutToolbox"
+	},
+    
+    
+    
+    // FLASHLIGHTS
+    /*
+        {
+		name: "Utility Flashlight",
+		image: "../../images/DbD/items/flashlightUtility.png",
+		id: "Item_Camper_Flashlight03"
+	},
+    {
+		name: "Sport Flashlight",
+		image: "../../images/DbD/items/flashlightSport.png",
+		id: "Item_Camper_Flashlight02"
+	},
+    */
 	{
 		name: "Flashlight",
 		image: "../../images/DbD/items/flashlight.png",
